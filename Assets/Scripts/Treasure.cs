ba099a5e19ab2c58708d71daa0146ca31083a0d6
Marcus Guimaraes <mguimaraes42@gmail.com>
2016-07-18 00:01:20 -0400

    Final commit

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -6,51 +6,59 @@ using UnityEngine.UI;
 public class Treasure : MonoBehaviour {
 	//Attach this class to the goal, and add waypoints in the Inspector
 
-	//TODO: code compass, recolor door, design level
-
 	public Text message;
 	public Transform player;
 	public Transform guide;
 	public Transform door;
+	public Transform openDoor;
 	public Transform key;
 	public Transform compass;
 	public static float activeDistance = .5f;
 
 	private bool didPlayerWin = false;
 	private bool foundKey = false;
+	private bool hasMoved = false;
 
 	// Update is called once per frame
 	void Update () {
 
-		if ( isNear(Player.startPos) ){
+		if ( !hasMoved && isNear(Player.startPos) ){
 			message.text = "Find the treasure!";
 		}
 
-		else if ( isNear(guide.position)) { //near Megaman
-			message.text = "\"Find the compass, then the key, and the treasure will be yours\"";
+		else if ( isNear(guide.position) ) { //near Megaman
+			message.text = "\"Find the key and regain your weapon\"";
 		}
 
-		else if ( !foundKey && isNear(door.position)) { //near locked door door
+		else if ( !foundKey && door.gameObject.activeInHierarchy && isNear(door.position)) { //near locked door
 			message.text = "Door locked!";
 		}
 
-		else if ( isNear(key.position)) { //by key
+		else if ( isNear(key.position) ) { //by key
 			message.text = "Found key!";
 			if (!foundKey) {//if this is the first frame the player is in contact with the key
 				door.gameObject.SetActive(false);
+				key.gameObject.SetActive(false);
+				openDoor.gameObject.SetActive(true);
 			}
 			foundKey = true;
 		}
 
-		else if ( isNear(transform.position) ) { //near treasure
-			message.text = "Press [SPACE] to get treasure and win!";
-			if (Input.GetKeyDown(KeyCode.Space))
-				didPlayerWin = true;
-			else if (didPlayerWin)
-				message.text = "You got the treasure!  You win!!!!";
+		else if ( isNear(compass.position) ) { //by compass
+			message.text = "The key is to the west";
 		}
 
-		else {
+		else if ( isNear(transform.position) ) { //near treasure
+			message.text = "Press [SPACE] to get Disc and win!";
+			if (Input.GetKeyDown(KeyCode.Space)){
+				didPlayerWin = true;
+			}
+			else if (didPlayerWin)
+				message.text = "Congratulations, you win!!!!";
+		}
+
+		else { //not near anything
+			hasMoved = true;
 			message.text = "";
 		}
 	}
