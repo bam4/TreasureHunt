3d296813bcbd468244c1ea600a9d1242d1e6148e
Brett Moody <brettmoody@me.com>
2016-07-19 15:31:57 -0400

    New Art Assets

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -1,38 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-using UnityEngine.UI;
+using UnityEngine.UI; // Allows access to UI elements in code
 
 public class Treasure : MonoBehaviour {
 	//Attach this class to the goal, and add waypoints in the Inspector
 
-	public Text message;
-	public Transform player;
-	public Transform guide;
-	public Transform door;
-	public Transform openDoor;
-	public Transform key;
-	public Transform compass;
-	public static float activeDistance = .5f;
+	public Text message; // This is the "Text Box" which contains the string that is outputted to the player. This is the Text UI object
+	public Transform player; // This is the position of the player, which can be accessed in the inspector.
+	public Transform guide; // This is the character that provides advice to the player.
+	public Transform door; // This is the position of the door that blocks the key.
+	public Transform openDoor; // This is the position of the openDoor that blocks the key.
+	public Transform key; // This is the position of one of the keys.
+	public Transform compass; // This is the position of the compass.
+	public static float activeDistance = .5f; // 
 
-	private bool didPlayerWin = false;
-	private bool foundKey = false;
-	private bool hasMoved = false;
+	private bool didPlayerWin = false; // This would allow the player to win the game.
+	private bool foundKey = false; // This would allow the player to open the door.
+	private bool hasMoved = false; // This notices player movement
 
 	// Update is called once per frame
 	void Update () {
+
+		// If the player has not moved and they are close to the start position of the game, show the below text.
 
 		if ( !hasMoved && isNear(Player.startPos) ){
 			message.text = "Find the treasure!";
 		}
 
+		// If they are near the guide, play the text below.
+
 		else if ( isNear(guide.position) ) { //near Megaman
 			message.text = "\"Find the key and regain your weapon\"";
 		}
 
+		// If they have not found the key and the door game object is active and the player is near the door, show the text below.
+
 		else if ( !foundKey && door.gameObject.activeInHierarchy && isNear(door.position)) { //near locked door
 			message.text = "Door locked!";
 		}
+
+		// If they player is near the key, show the text, turn off the closed door, turn on the open door, turn off the key.
 
 		else if ( isNear(key.position) ) { //by key
 			message.text = "Found key!";
@@ -43,6 +50,8 @@ public class Treasure : MonoBehaviour {
 			}
 			foundKey = true;
 		}
+
+
 
 		else if ( isNear(compass.position) ) { //by compass
 			message.text = "The key is to the west";
