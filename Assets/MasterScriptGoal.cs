1c319161307e4727951c43402c3ca348ccf62168
Brett Moody <brettmoody@me.com>
2016-07-20 16:34:23 -0400

    Fixed all doors and UI. Add characters, workbenches. Did not code their function.

--- Assets/MasterScriptGoal.cs
+++ Assets/MasterScriptGoal.cs
@@ -14,6 +14,7 @@ public class MasterScriptGoal : MonoBehaviour {
 	public Transform roman;
 
 
+	public Image panel;
 
 	public Transform greyDoorOne; // This is the position of the door that blocks the entrance to the Zelda section.
 	public Transform greyDoorTwo;
@@ -37,6 +38,7 @@ public class MasterScriptGoal : MonoBehaviour {
 	public Transform MarioObject;
 	public Transform LOTRObject;
 	public Transform GTAObject;
+	public Transform theStranger;
 
 	public static float activeDistance = 1f; // This sets a single distance for object activation.
 
@@ -58,18 +60,22 @@ public class MasterScriptGoal : MonoBehaviour {
 	private bool foundKey = false; // This would allow the player to open the door.
 	private bool hasMoved = false; // This notices player movement
 
+
+	void Start () {
+		
+	}
+	
+
 	// Update is called once per frame
 	void Update () {
 
 
+
+		panel.GetComponent<Image>().enabled = true;
 		message.text = "";
+	
 
-		// If the player has not moved and they are close to the start position of the game, show the below text.
-		if (!hasMoved && isNear (Player.startPos)) {
-			message.text = "Find the treasure!";
-
-
-		} else if (isNear (toad.position)) {
+		if (isNear (toad.position)) {
 			message.text = "Press [Space] to talk to this key and open the gray door in front of you!";
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				greyDoorOne.gameObject.SetActive (false);
@@ -90,8 +96,15 @@ public class MasterScriptGoal : MonoBehaviour {
 			}
 		}
 
+		else if (isNear (theStranger.position) && theStranger.gameObject.active) {
+			message.text = "Press [Space] to pick up this strange book.";
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				theStranger.gameObject.SetActive (false);
+			}
+		}
 
-		else if (isNear(blueKey.position)) {
+
+		else if (isNear(blueKey.position) && !hasBlueKey) {
 			message.text = "Press [Space] to pick up this key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				hasBlueKey = true;
@@ -99,14 +112,14 @@ public class MasterScriptGoal : MonoBehaviour {
 			}
 		}
 
-		else if (isNear(blueDoor.position) && hasBlueKey) {
+		else if (isNear(blueDoor.position) && hasBlueKey && blueDoor.gameObject.activeInHierarchy) {
 		message.text = "Press [Space] to open this door with your key!";
 		if (Input.GetKeyDown(KeyCode.Space)){
 			blueDoor.gameObject.SetActive(false);
 			}
 		}
 
-		else if (isNear(orangeKey.position)) {
+		else if (isNear(orangeKey.position) && !hasOrangeKey) {
 		message.text = "Press [Space] to pick up this key!";
 		if (Input.GetKeyDown(KeyCode.Space)){
 			hasOrangeKey = true;
@@ -114,14 +127,14 @@ public class MasterScriptGoal : MonoBehaviour {
 			}
 		}
 
-		else if (isNear(orangeDoor.position) && hasOrangeKey) {
+		else if (isNear(orangeDoor.position) && hasOrangeKey && orangeDoor.gameObject.activeInHierarchy) {
 		message.text = "Press [Space] to open this door with your key!";
 		if (Input.GetKeyDown(KeyCode.Space)){
 				orangeDoor.gameObject.SetActive(false);
 			}
 		}
 
-		else if (isNear(purpleKey.position)) {
+		else if (isNear(purpleKey.position) && !hasPurpleKey) {
 		message.text = "Press [Space] to pick up this key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				hasPurpleKey = true;
@@ -129,14 +142,14 @@ public class MasterScriptGoal : MonoBehaviour {
 			}
 		}
 
-		else if (isNear (purpleDoor.position) && hasPurpleKey) {
+		else if (isNear (purpleDoor.position) && hasPurpleKey && purpleDoor.gameObject.activeInHierarchy) {
 		message.text = "Press [Space] to open this door with your key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				purpleDoor.gameObject.SetActive(false);
 			}
 		}
 
-		else if (isNear(greenKey.position)) {
+		else if (isNear(greenKey.position) && !hasGreenKey) {
 			message.text = "Press [Space] to pick up this key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				hasGreenKey = true;
@@ -144,14 +157,14 @@ public class MasterScriptGoal : MonoBehaviour {
 			}
 		}
 
-		else if (isNear (greenDoor.position) && hasGreenKey) {
+		else if (isNear (greenDoor.position) && hasGreenKey && greenDoor.gameObject.activeInHierarchy) {
 			message.text = "Press [Space] to open this door with your key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				greenDoor.gameObject.SetActive(false);
 			}
 		}
 
-		else if (isNear(redKey.position)) {
+		else if (isNear(redKey.position) && !hasRedKey) {
 			message.text = "Press [Space] to pick up this key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				hasRedKey = true;
@@ -159,7 +172,7 @@ public class MasterScriptGoal : MonoBehaviour {
 			}
 		}
 
-		else if (isNear(redDoor.position) && hasRedKey) {
+		else if (isNear(redDoor.position) && hasRedKey && redDoor.gameObject.activeInHierarchy) {
 			message.text = "Press [Space] to open this door with your key!";
 			if (Input.GetKeyDown(KeyCode.Space)){
 				redDoor.gameObject.SetActive(false);
@@ -169,45 +182,31 @@ public class MasterScriptGoal : MonoBehaviour {
 
 		// If they are near the guide, play the text below.
 
-		else if ( isNear(toad.position) ) { //near Megaman
+		else if (isNear(toad.position) ) { //near Megaman
 			message.text = "\"Find the key and regain your weapon\"";
 		}
 
 		// If they have not found the key and the door game object is active and the player is near the door, show the text below.
 
-		else if ( !foundKey && grayDoor.gameObject.activeInHierarchy && isNear(grayDoor.position)) { //near locked door
-			message.text = "Door locked!";
-		}
-
-		// If they player is near the key, show the text, turn off the closed door, turn on the open door, turn off the key.
-
-		else if ( isNear(blueKey.position) ) { //by key
-			message.text = "Found key!";
-			if (!foundKey) {//if this is the first frame the player is in contact with the key
-				grayDoor.gameObject.SetActive(false);
-				blueKey.gameObject.SetActive(false);
-				grayDoor.gameObject.SetActive(true);
-			}
-			foundKey = true; // Give the player the "key."
-		}
-
-	
-
 		// If the player is near the treasure, play the text prompting them for a "Space" input. They will win!
 
-		else if ( isNear(transform.position) ) { //near treasure
-			message.text = "Press [SPACE] to get Disc and win!";
-			if (Input.GetKeyDown(KeyCode.Space)){
-				didPlayerWin = true;
-			}
-			else if (didPlayerWin)
-				message.text = "Congratulations, you win!!!!";
-		}
+//		else if ( isNear(transform.position) ) { //near treasure
+//			message.text = "Press [SPACE] to get Disc and win!";
+//			if (Input.GetKeyDown(KeyCode.Space)){
+//				didPlayerWin = true;
+//			}
+//			else if (didPlayerWin)
+//				message.text = "Congratulations, you win!!!!";
+//		}
 
 		else { //not near anything
-			hasMoved = true;
-			message.text = "";
-			// GetComponent<Panel>().SetActive(false);	
+			// hasMoved = true;
+			panel.GetComponent<Image>().enabled = false;
+		}
+
+		if (Time.time < 5) {
+			panel.GetComponent<Image>().enabled = true;
+			message.text = "Find the treasure!";
 		}
 	}
 
